25b1c11ae7d6b1ff9e10ff07a3d4d73ff9e5a042
Arnaud DEBEC <debec.arnaud@gmail.com>
2017-11-17 09:58:17 +0100

    Simplier connexion

--- core/urls.py
+++ core/urls.py
@@ -4,17 +4,8 @@ from . import views
 
 app_name = 'core'
 urlpatterns = [
-    # ex: /signin
     url(r'^signup/$', views.signup, name='signup'),
-    url(r'^account_activation_sent/$', views.account_activation_sent, name='account_activation_sent'),
-    url(r'^activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/$', views.activate, name='activate'),
-
-    # ex: /polls/
-    # url(r'^$', views.IndexView.as_view(), name='index'),
-    # ex: /polls/5/
-    # url(r'^(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
-    # ex: /polls/5/results/
-    # url(r'^(?P<pk>[0-9]+)/results/$', views.ResultsView.as_view(), name='results'),
-    # ex: /polls/5/vote/
-    # url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
+    url(r'^home/$', views.signup, name='home'),
+    url(r'^login/$', views.signup, name='login'),
+    url(r'^logout/$', views.signup, name='logout')
 ]
